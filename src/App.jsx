1ab0649b87e0af5316f28f342cb846979e57570a
masome hosine <hosine.masome@gmail.com>
2024-01-20 02:44:55 +0330

    Installation react-router-dom and antd

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import PageContent from './Components/PageContent'
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <PageContent />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
